f7266741475da3e3a2bf3cae0863c2df4d2fe831
Shuyuan <kukutou_mikiya@outlook.com>
2023-03-31 22:43:17 -0700

    release: 1.0

--- src/app/search/detail-card/detail-card.module.ts
+++ src/app/search/detail-card/detail-card.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { MatTabsModule } from '@angular/material/tabs';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { GoogleMapsModule } from '@angular/google-maps'
 import { EventDetailComponent } from './event-detail/event-detail.component';
@@ -22,7 +23,8 @@ import { VenueDetailComponent } from './venue-detail/venue-detail.component';
   imports: [
     CommonModule,
     ModalModule.forRoot(),
-    MatTabsModule,  
+    MatTabsModule, 
+    BrowserAnimationsModule, 
     CarouselModule.forRoot(),
     GoogleMapsModule,
     MatProgressSpinnerModule
